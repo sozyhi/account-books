0f3f27f8b77431db38235b6549203b741145d739
sozyhi <sozyhi@outlook.com>
2020-02-15 20:26:25 +0800

    按照vue-class-component文档使用失败

--- src/components/Types.vue
+++ src/components/Types.vue
@@ -1,36 +1,71 @@
 <template>
   <div>
     <ul class="type">
-      <li class="selected">支出</li>
-      <li>收入</li>
+      <li :class="type === '-' && 'selected'" @click="selectType('-')">支出</li>
+      <li :class="type === '+' && 'selected'" @click="selectType('+')">收入</li>
     </ul>
-  </div></template>
+  </div>
+</template>
 
 <script lang="ts">
-  export default {
-    name: 'Types'
-  };
+  import Vue from 'vue';
+  import {Component} from 'vue-property-decorator';
+
+  @Component({
+  props:{
+    propMessage:String
+  }
+  })
+  export default class Types extends Vue {
+    type = '-';
+    selectType(type: string) {
+      if (type !== '-' && type !== '+') {
+        throw new Error('type is unknown');
+      }
+      this.type = type;
+    }
+
+  // export default {
+  //   name: 'Types',
+  //   data(){
+  //     return{
+  //       type:'-'
+  //     }
+  //   },
+  //   methods:{
+  //     selectType(type){
+  //       if(type !=='-' && type !=='+'){
+  //         throw new Error('type is unknown')
+  //       }
+  //       this.type = type
+  //     }
+  //   }
+  }
 </script>
 
 <style lang="scss" scoped>
   @import "~@/assets/style/uniform-color.scss";
-  .type{
+
+  .type {
     @extend %outerShadow;
     display: flex;
     font-size: 20px;
-    >li{
+
+    > li {
       position: relative;
       color: $color-ordinary;
       display: flex;
-      justify-content:space-around;
+      justify-content: space-around;
       width: 50vw;
-      padding:10px 0;
-      &.selected{
-        color:$color-selected-word;
-        &::after{
-          content:'';
+      padding: 10px 0;
+
+      &.selected {
+        color: $color-selected-word;
+
+        &::after {
+          content: '';
           position: absolute;
-          bottom:0;
+          bottom: 0;
           left: 0;
           background: $color-selected-icon;
           height: 4px;
